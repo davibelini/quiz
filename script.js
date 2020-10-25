468bdb4eab7cf94f2fab7baa6f6e747af95bdae2
dado <dev.dado.belini@gmail.com>
2020-10-24 23:48:12 -0300

    style and data

---
+++ script.js
@@ -0,0 +1,47 @@
+const quizData = [
+  {
+    question: "is 45 a prime number?",
+    a1: "yes",
+    a2: "no",
+    correct: 'a2'
+  },
+  {
+    question: "what is the most commonly diagnosed cancer in men?",
+    a1: "kidney cancer",
+    a2: "breast cancer",
+    a3: "prostate cancer",
+    a4: "skin cancer"
+  },
+  {
+    question: "how many hearts does a octopus have?",
+    a1: "1",
+    a2: "2",
+    a3: "3",
+    a4: "4",
+    correct: "a3"
+  },
+  {
+    question: "how much of a jelly fish is water?",
+    a1: "24.5%",
+    a2: "35%",
+    a3: "67.376%",
+    a4: "90%",
+    correct: "a4"
+  },
+  {
+    question: "the day on which the Sun’s direct rays cross the celestial equator is called:",
+    a1: "the eliptic",
+    a2: "the aphelion",
+    a3: "the solstice",
+    a4: "the equinox",
+    correct: "a4"
+  },
+  {
+    question: "who invented the telescope?",
+    a1: "Johannes Kepler",
+    a2: "Galileu Galilei",
+    a3: "Hans Lippershey",
+    a4: "Isaac Newton",
+    correct: "a3"
+  }
+]
